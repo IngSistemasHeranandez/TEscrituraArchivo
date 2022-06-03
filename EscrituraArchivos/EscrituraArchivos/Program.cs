efc7875257ba433726273c8f0e9216fc187e1e1e
IngSistemasHeranandez <105814871+IngSistemasHeranandez@users.noreply.github.com>
2022-06-03 11:14:06 -0700

    Add files via upload

---
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EscrituraArchivos
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Ejemplo.txt",true);
+            //si el archivo no exisite, lo creara
+            //Si el archivo exisiste, escribira en el
+
+            String[] Lines =
+                {"Esto es la primera linea",
+                 "Esto es la segunda Linea",
+                 "Esto es la tercera Linea"};
+
+            //Recorrer el arreglo para escribir las lineas en el archivo
+            foreach(string Line in Lines)
+            {
+                //Escribir en el archivo
+                sw.WriteLine(Line);
+            }
+
+            //Cerrar el programa
+            sw.Close();
+
+            Console.WriteLine("Escribiendo....");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticaEscrituraArchivo/PracticaEscrituraArchivo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace PracticaEscrituraArchivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Ejemplo.txt", true);
+            //si el archivo no exisite, lo creara
+            //Si el archivo exisiste, escribira en el
+
+            //Variable 
+            string Nombre;
+
+            //Asignacion de variable
+            Console.Write("Ingresa un Nombre: ");
+            Nombre=Console.ReadLine();
+
+           
+            String[] Lines =
+                {Nombre};
+
+            //Recorrer el arreglo para escribir las lineas en el archivo
+            foreach (string Line in Lines)
+            {
+                //Escribir en el archivo
+                sw.WriteLine(Line);
+            }
+
+            //Cerrar el programa
+            sw.Close();
+
+            Console.WriteLine("Escribiendo....");
+            Console.ReadLine();
+        }
+    }
+}
